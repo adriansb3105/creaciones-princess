62efdfb512cb661306e314fa0c420e81d5113bc0
Adrian Serrano <adriansb3105@gmail.com>
2024-08-28 00:21:42 -0600

    Add logic to the images disply

--- src/app/(pages)/agendas/page.jsx
+++ src/app/(pages)/agendas/page.jsx
@@ -5,7 +5,7 @@ for (let i = 0; i < 36; i++) {
     agendas.push(
     {
         name: 'Agenda',
-        img: 'agendas.jpg'
+        image: 'agendas.jpg'
     }
     )
 }
@@ -13,14 +13,14 @@ for (let i = 0; i < 36; i++) {
 export default function Agendas(params) {
     return (
         <section>
-            <h1 className="bg-[#ffe5ff] text-center text-5xl font-bold text-gray-400">Agendas</h1>
+            <h1 className="bg-[#ffe5ff] text-center text-5xl font-bold text-[#94E8D5]">Agendas</h1>
             
             <div className='bg-[#ffe5ff] py-5 px-10 sm:py-5 sm:px-16 columns-4 gap-3 mx-auto space-y-3'>
             {
                 agendas.map((agenda) => {
                 return (
                     <div key={agenda.name} className={`bg-gray-200 break-inside-avoid`}>
-                        <img src={`/img/${agenda.img}`} alt={agenda.name} />
+                        <img src={`/img/${agenda.image}`} alt={agenda.name} />
                     </div>
                 )})
             }
