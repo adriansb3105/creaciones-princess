0c51424b4b2ec50ba8930e87d9109f6caf2cfc22
Adrian Serrano <adriansb3105@gmail.com>
2024-08-27 22:42:59 -0600

    Add key value to the iterable div

--- src/app/(pages)/agendas/page.jsx
+++ src/app/(pages)/agendas/page.jsx
@@ -1,44 +1,30 @@
-'use client'
+const agendas = [
+]
 
-import 'two-up-element'
+for (let i = 0; i < 36; i++) {
+    agendas.push(
+    {
+        name: 'Agenda',
+        img: 'agendas.jpg'
+    }
+    )
+}
 
 export default function Agendas(params) {
     return (
-        <>
+        <section>
             <h1 className="bg-[#ffe5ff] text-center text-5xl font-bold text-gray-400">Agendas</h1>
-
+            
             <div className='bg-[#ffe5ff] py-5 px-10 sm:py-5 sm:px-16 columns-4 gap-3 mx-auto space-y-3'>
-                
-            <two-up>
-            <img src="/img/agendas.jpg" alt="Agenda" />
-            <img src="/img/agendas.jpg" alt="Agenda" />
-            </two-up>
-
-
-                {/*<div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>*/}
+            {
+                agendas.map((agenda) => {
+                return (
+                    <div key={agenda.name} className={`bg-gray-200 break-inside-avoid`}>
+                        <img src={`/img/${agenda.img}`} alt={agenda.name} />
+                    </div>
+                )})
+            }
             </div>
-        </>
+        </section>
     )
 }
