80bd9544767fbb681e2f33e6e5d0a2ac8491fba9
Adrian Serrano <adriansb3105@gmail.com>
2024-08-27 20:06:46 -0600

    Fix the font in the main page, improve the grid template, improve the responsive font size

--- src/app/(pages)/agendas/page.jsx
+++ src/app/(pages)/agendas/page.jsx
@@ -1,9 +1,21 @@
-export default async function Agendas(params) {
+'use client'
+
+import 'two-up-element'
+
+export default function Agendas(params) {
     return (
         <>
             <h1 className="bg-[#ffe5ff] text-center text-5xl font-bold text-gray-400">Agendas</h1>
 
             <div className='bg-[#ffe5ff] py-5 px-10 sm:py-5 sm:px-16 columns-4 gap-3 mx-auto space-y-3'>
+                
+            <two-up>
+            <img src="/img/agendas.jpg" alt="Agenda" />
+            <img src="/img/agendas.jpg" alt="Agenda" />
+            </two-up>
+
+
+                {/*<div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
@@ -25,8 +37,7 @@ export default async function Agendas(params) {
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
                 <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
-                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>
+                <div className={`bg-gray-200 break-inside-avoid`}><img src="/img/agendas.jpg" alt="Agenda" /></div>*/}
             </div>
         </>
     )
---
+++ src/app/page.jsx
@@ -0,0 +1,58 @@
+// import Image from "next/image";
+
+import Link from "next/link"
+
+const items = [
+  {
+    name: 'Agendas',
+    link: 'agendas',
+    image: 'agendas.jpg',
+    cover: 'cover'
+  },
+  {
+    name: 'Artesanías',
+    link: 'artesanias',
+    image: 'artesanias.jpg',
+    cover: 'cover'
+  },
+  {
+    name: 'Decoraciones y Eventos',
+    link: 'eventos',
+    image: 'eventos.jpg',
+    cover: 'cover'
+  },
+  {
+    name: 'Pastelería',
+    link: 'pasteleria',
+    image: 'pasteleria.jpg',
+    cover: 'contain'
+  },
+  {
+    name: 'Repostería',
+    link: 'reposteria',
+    image: 'reposteria.png',
+    cover: 'cover'
+  },
+  {
+    name: 'Scrapbook',
+    link: 'scrapbook',
+    image: 'scrapbook.jpg',
+    cover: 'cover'
+  }
+]
+
+export default function Home() {
+
+  return (
+    <section className='bg-[#ffe5ff] h-min md:h-screen p-10 sm:p-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 lg:gap-6'>
+      {
+        items?.map((item) => {
+          return (
+          <Link href={item.link} className={`font-[Allura-Regular] font-bold bg-[url('/img/${item.image}')] bg-${item.cover} aspect-video text-white flex items-center justify-center text-center text-3xl lg:text-4xl md:text-2xl xl:text-4xl 2xl:text-5xl py-4 rounded-lg`}>
+            {item.name}
+          </Link>
+        )})
+      }
+    </section>
+  );
+}
